5e8b330d467acd3d8d093ace6f5cb8e9d4ec2109
“Murgana” <fabrizio.murgana@dfa.unict.it>
2022-05-05 15:51:27 +0200

    coded trafo

---
+++ Quark_Photon_Vertex/include/coeff.hh
@@ -0,0 +1,23 @@
+#pragma once
+#include <math.h>
+
+// your code
+using namespace std
+
+double a0 (int i)
+{
+ if (i==1)
+  { 
+   return sqrt(2);
+  }
+  
+  else if (i==7 || i==10)
+   {
+    return 1;
+   }
+   
+  else 
+   {
+    return 0;
+   }
+}
