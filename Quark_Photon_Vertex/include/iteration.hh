f6329a9df31fa7b0be3a2a45ecf0be042a77608b
sthagel <sthagel@uw.edu>
2022-05-10 14:20:37 +0200

    WIP

--- Quark_Photon_Vertex/include/iteration.hh
+++ Quark_Photon_Vertex/include/iteration.hh
@@ -1,11 +1,18 @@
 #include <vector>
 #include <complex>
 #include "coeff.hh"
+#include "Kernels_G.hh"
+#include "Kernels_K.hh"
 
 typedef std::vector<std::complex<double>> vec_cmplx;
 typedef std::vector<vec_cmplx> mat_cmplx;
 typedef std::vector<mat_cmplx> tens_cmplx;
 typedef std::vector<double> vec_double;
+
+// TODO: Write this function and put it in an appropriate place.
+double interpolate(const vec_double &grid, const double x) {
+    return 1.0;
+}
 
 void iterate_a_and_b(const vec_double &q_grid, const vec_double &z_grid, const vec_double &k_grid, const vec_double & y_grid)
 {
@@ -16,7 +23,7 @@ void iterate_a_and_b(const vec_double &q_grid, const vec_double &z_grid, const v
     const unsigned int z_steps = z_grid.size();
     const unsigned int q_steps = q_grid.size();
     const unsigned int y_steps = y_grid.size();
-    const vec_cmplx temp2(z_steps, 0.0);
+    const vec_cmplx temp2(z_steps, 1.0);
     const mat_cmplx temp(k_steps, temp2);
     tens_cmplx a(n_structs, temp);
     tens_cmplx b(n_structs, temp);
@@ -29,48 +36,29 @@ void iterate_a_and_b(const vec_double &q_grid, const vec_double &z_grid, const v
     // Generate the K kernel, so we don't have to do a lot of function calls
     // Inside the iterations
     const unsigned int k_dimension = n_structs * n_structs * k_steps * k_steps * z_steps * z_steps * y_steps * q_steps;
-    vec_double k_kernel(k_dimension, 0.0);
-
-    // TODO: Parallelize me!
-    // ... but I might not even be needed ...
-    for (int super_idx = 0; super_idx < k_dimension; ++super_idx) {
-        const unsigned int i = k_dimension % n_structs;
-        const unsigned int j = (k_dimension / n_structs) % n_structs;
-        const unsigned int k_idx = (k_dimension / (n_structs * n_structs)) % k_steps;
-        const unsigned int k_prime_idx = (k_dimension / (n_structs * n_structs * k_steps)) % k_steps;
-        const unsigned int z_idx = (k_dimension / (n_structs * n_structs * k_steps * k_steps)) % z_steps;
-        const unsigned int z_prime_idx = (k_dimension / (n_structs * n_structs * k_steps * k_steps * z_steps)) % z_steps;
-        const unsigned int y_idx = (k_dimension / (n_structs * n_structs * k_steps * k_steps * z_steps * z_steps)) % y_steps;
-        const unsigned int q_idx = (k_dimension / (n_structs * n_structs * k_steps * k_steps * z_steps * z_steps * y_steps)) % q_steps;
-
-        const double k = k_grid[k_idx];
-        const double k_prime = k_grid[k_prime_idx];
-        const double z = z_grid[z_idx];
-        const double z_prime = z_grid[z_prime_idx];
-        const double y = y_grid[y_idx];
-        const double q = q_grid[q_idx];
-
-        // TODO: Transform the variables to the ones used in the K kernel functions
-
-        k_kernel[super_idx] = 0.0; // TODO: Call the K kernel functions here
-    }
 
     while (current_acc > target_acc && max_steps > current_step) {
         ++current_step;
-        // TODO: Loop over Q
-        // Iterate the a_i's
-        for (int i = 0; i < n_structs; ++i) {
-            for (int z_idx = 0; z_idx < z_steps; ++z_idx) {
-                const double z = z_grid[z_idx];
-                for (int k_idx = 0; k_idx < k_steps; ++k_idx) {
-                    const double k = k_grid[k_idx];
+        // For now this will be for a fixed value of q_sq. TODO: Generalize this
+        const double q_sq = 0.0;
+
+        // loop over i
+        for (unsigned int i = 0; i < n_structs; ++i) {
+            // loop over k
+            for (unsigned int k_idx = 0; k_idx < k_steps; ++k_idx) {
+                const double k_sq = k_grid[k_idx];
+                // loop over z
+                for (unsigned int z_idx = 0; z_idx < z_steps; ++z_idx) {
+                    const double z = z_grid[z_idx];
+
+                    // Initialize them with the inhomogeneous term
                     a[i][k_idx][z_idx] = z_2 * a0(i);
-                    for (int j = 0; j < n_structs; ++j) {
-                        a[i][k_idx][z_idx] += Integrate[
-                                g((k-kprime)*(k-kprime)) * K(i, j, k*k, kprime*kprime, z, zprime, y, q*q) * b[j](kprime*kprime, zprime, q*q)
-                                ,kprime, zprime
-                                ] // TODO: Finish this up
+
+                    // loop over j
+                    for (unsigned int j = 0; j < n_structs; ++j) {
+
                     }
+
                 }
             }
         }
