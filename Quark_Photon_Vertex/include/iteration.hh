3a364ebcbe26b4ab3c8494f602c79eadc6ecea2e
sthagel <sthagel@uw.edu>
2022-05-11 15:49:10 +0200

    Adds factor of 2pi from the phi iteration

--- Quark_Photon_Vertex/include/iteration.hh
+++ Quark_Photon_Vertex/include/iteration.hh
@@ -99,7 +99,7 @@ void iterate_a_and_b(const vec_double &q_grid, const vec_double &z_grid, const v
                                     lInterpolator2d interpolate2d(k_grid, z_grid, b[q_iter][j]);
                                     const auto b_j = interpolate2d(k_prime_sq, z_prime);
 
-                                    return gl * k_kernel.get(i, j) * b_j;
+                                    return 2.0 * M_PI * gl * k_kernel.get(i, j) * b_j;
                                 };
 
                                 // Evaluate the integral
