3706977c37d70da54ff911c0b5fc5078c8b8082d
sthagel <sthagel@uw.edu>
2022-05-11 17:43:30 +0200

    Sets order to 10

--- Quark_Photon_Vertex/Simulation.cpp
+++ Quark_Photon_Vertex/Simulation.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
   auto f = [](const double& k, const double& Q){ return 2.; };
   std::cout << "We get " << qint(f, 0., 1., 0., 2.) << "\n";*/
 
-    constexpr unsigned int order = 4;
+    constexpr unsigned int order = 10;
     static_assert(order % 2 == 0);
     std::vector<double> k_grid(order);
     std::vector<double> q_grid(order);
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
     q_grid = linearMapTo(q_grid, 0., double(q_grid.size()-1), 1e-3, 3.);
 
     for (unsigned i = 0; i < q_grid.size(); ++i)
-      std::cout << q_grid[i] << " q ";
+        std::cout << q_grid[i] << " q ";
     std::cout << "\n";
 
     LegendrePolynomial<order> lp;
