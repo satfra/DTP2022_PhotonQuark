58e4acda156e75f6a59da7caae786202d5923fdd
sthagel <sthagel@uw.edu>
2022-05-11 14:33:53 +0200

    Uses more meaningful grids in main

--- Quark_Photon_Vertex/Simulation.cpp
+++ Quark_Photon_Vertex/Simulation.cpp
@@ -13,10 +13,17 @@ int main(int argc, char *argv[]) {
   auto f = [](const double& k, const double& Q){ return 2.; };
   std::cout << "We get " << qint(f, 0., 1., 0., 2.) << "\n";*/
 
-    constexpr unsigned int order = 2;
+    constexpr unsigned int order = 5;
+    std::vector<double> k_grid(order);
+    std::vector<double> q_grid(order);
+    for (int i = 0; i < order; ++i) {
+        k_grid[i] = 1.0 * i + 1e-3;
+        q_grid[i] = 1.0 * i + 1e-3;
+    }
     LegendrePolynomial<order> lp;
-    const auto grid1 = lp.zeroes();
-    iterate_a_and_b(grid1, grid1, grid1, grid1);
+    std::vector<double> z_grid = lp.zeroes();
+    const std::vector<double>& y_grid = z_grid;
+    iterate_a_and_b(q_grid, z_grid, k_grid, y_grid);
 
     return 0;
 }
