90d188e335bf8609f45d289ca20e59774168e1ec
sthagel <sthagel@uw.edu>
2022-05-11 14:25:28 +0200

    Adds iteration to main function
    Thus far for testing/debugging purposes only.

--- Quark_Photon_Vertex/Simulation.cpp
+++ Quark_Photon_Vertex/Simulation.cpp
@@ -5,19 +5,18 @@
 #include <LinearInterpolate.hh>
 #include <iteration.hh>
 
-int main(int argc, char* argv[])
-{
-  // constexpr unsigned order_Q = 10;
-  // constexpr unsigned order_k = 10;
+int main(int argc, char *argv[]) {
+    // constexpr unsigned order_Q = 10;
+    // constexpr unsigned order_k = 10;
 /*
   qIntegral2d<LegendrePolynomial<order_k>, LegendrePolynomial<order_Q>> qint;
   auto f = [](const double& k, const double& Q){ return 2.; };
   std::cout << "We get " << qint(f, 0., 1., 0., 2.) << "\n";*/
 
-  std::vector<double> x{1,2,3,4,5};
-  std::vector<double> f{1,9,3,4,5};
-  lInterpolator lpol(x, f);
-  std::cout << "Interp is " <<lpol(1.5) << "\n";
+    constexpr unsigned int order = 2;
+    LegendrePolynomial<order> lp;
+    const auto grid1 = lp.zeroes();
+    iterate_a_and_b(grid1, grid1, grid1, grid1);
 
-  return 0;
+    return 0;
 }
