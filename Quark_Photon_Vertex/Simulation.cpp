cfa41b93b63aad4f36a6c31bf9a0da429f0fa1da
Franz Richard Sattler <sattler@thphys.uni-heidelberg.de>
2022-05-13 17:02:30 +0200

    oh mo

--- Quark_Photon_Vertex/Simulation.cpp
+++ Quark_Photon_Vertex/Simulation.cpp
@@ -8,37 +8,31 @@
 
 int main(int argc, char *argv[]) 
 {
-    constexpr unsigned order_k = 8;
-    constexpr unsigned order_q = 1;
-    constexpr unsigned order_z = 2;
-    static_assert(order_z % 2 == 0);
-    std::vector<double> k_grid(order_k);
-    std::vector<double> q_grid(order_q);
+  constexpr unsigned order_k = 8;
+  constexpr unsigned order_q = 1;
+  constexpr unsigned order_z = 2;
+  static_assert(order_z % 2 == 0);
 
-    std::iota(k_grid.begin(), k_grid.end(), 0);
-    std::iota(q_grid.begin(), q_grid.end(), 0);
+  std::vector<double> k_grid(order_k);
+  std::vector<double> q_grid(order_q);
 
-    constexpr double L = 1e+3;
-    std::cout << "L=" << L << "\n";
-    constexpr double k_sq_min = 1e-6;
-    constexpr double k_sq_max = L*L;
-    k_grid = linearMapTo(k_grid, 0., double(k_grid.size()-1), k_sq_min, k_sq_max);
-    q_grid = linearMapTo(q_grid, 0., double(q_grid.size()-1), 1e-3, 3.);
+  std::iota(k_grid.begin(), k_grid.end(), 0);
+  std::iota(q_grid.begin(), q_grid.end(), 0);
 
-    q_grid[0] = 0.001;
+  constexpr double L = 1e+3;
+  std::cout << "L=" << L << "\n";
+  constexpr double k_sq_min = 1e-6;
+  constexpr double k_sq_max = L*L;
+  k_grid = linearMapTo(k_grid, 0., double(k_grid.size()-1), k_sq_min, k_sq_max);
+  q_grid = linearMapTo(q_grid, 0., double(q_grid.size()-1), 1e-3, 3.);
 
-    for (unsigned i = 0; i < q_grid.size(); ++i)
-        std::cout << q_grid[i] << " q ";
-    std::cout << "\n";
+  q_grid[0] = 1.;
 
+  LegendrePolynomial<order_z> lp;
+  const std::vector<double> z_grid = lp.zeroes();
+  const std::vector<double> y_grid = z_grid;
 
-    LegendrePolynomial<order_z> lp;
-    const std::vector<double> z_grid = lp.zeroes();
-    for (unsigned i = 0; i < z_grid.size(); ++i)
-      std::cout << z_grid[i] << " z ";
-    std::cout << "\n";
-    const std::vector<double> y_grid = z_grid;
-    iterate_a_and_b(q_grid, z_grid, k_grid, y_grid);
+  iterate_a_and_b(q_grid, z_grid, k_grid, y_grid);
 
-    return 0;
+  return 0;
 }
