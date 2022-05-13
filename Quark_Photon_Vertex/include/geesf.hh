7cd07454f256b7a7b97a5b559a170957b29fe934
sthagel <sthagel@uw.edu>
2022-05-13 16:49:54 +0200

    Fixes geesf.hh

--- Quark_Photon_Vertex/include/geesf.hh
+++ Quark_Photon_Vertex/include/geesf.hh
@@ -1,66 +1,77 @@
 #pragma once
+
 #include <cmath>
+#include <complex>
 
-// your code
-using namespace std
-
-double g1 (const double& a10, const double& a11, const double& z, const double& s)
+std::complex<double> g1(const std::complex<double> a10, const std::complex<double> a11, const double &z, const double &s)
 {
- return a10-(z/s)*a11;
+    return a10 - (z / s) * a11;
 }
 
-double g2 (const double& k, const double& a11, const double& z, const double& s)
+std::complex<double> g2(const double &k, const std::complex<double> a11, const double &z, const double &s)
 {
- return a11/(k*k*z*s);
+    return a11 / (k * k * z * s);
 }
 
-double g3 (const double& k, const double& a9, const double& z) //g3 it's actually immaginary and we shouldn't forget about that!!
+std::complex<double> g3(const double &k, const std::complex<double> a9,
+          const double &z) //g3 it's actually immaginary and we shouldn't forget about that!!
 {
- return a9/(k*z);
+    constexpr std::complex<double> II = {0.0, 1.0};
+    return - II * a9 / (k * z);
 }
 
-double g4 (const double& k, const double& a12, const double& z, const double& s, const double& Q) //also g4 is immaginary
+std::complex<double> g4(const double &k, const std::complex<double> a12, const double &z, const double &s, const double &Q) //also g4 is immaginary
 {
- return a12/(k*k*Q*s*z);
+    constexpr std::complex<double> II = {0.0, 1.0};
+    return II * a12 / (k * k * Q * s * z);
 }
 
-
-double f1 (const double Q, const double s, const double z, const double a1, const double a6, const double a7 , const double a10, const double a11)
+std::complex<double> f1(const double Q, const double s, const double z, const double a1, const double a6, const
+double a7, const double a10,
+   const double a11)
 {
-	return (a1 / (sqrt(2) + z * s * (a6 + a11) - z * z * a7 - s * s * a10) / (Q * Q * s * s);
-} 
-
-double f2 (const double Q, const double k, const double s, const double z, const double a2, const double a3, const double a8 , const double a12) // immaginari !!!!
-{
-	return -(a2 / (sqrt(2) - a8 + s / z * (a3/ (sqrt(2)) + a12 ) / (k * k * Q* Q * Q * s * s);
+    return (a1 / (std::sqrt(2.) + z * s * (a6 + a11) - z * z * a7 - s * s * a10)) / (Q * Q * s * s);
 }
 
-double f3 (const double Q, const double s, const double z, const double a2, const double a3) //immaginari
+std::complex<double> f2(const double Q, const double k, const double s, const double z, const double a2, const double
+a3, const double a8,
+   const double a12) // immaginari !!!!
 {
-	return (-a2 + z / s * a3) / (sqrt(2) * Q);
+    constexpr std::complex<double> II = {0.0, 1.0};
+    return -II * (a2 / std::sqrt(2.) - a8 + s / z * (a3 / sqrt(2.) + a12)) / (k * k * Q * Q * Q * s * s);
 }
 
-double f4 (const double Q, const double k, const double s, const double z, const double a4)
+std::complex<double> f3(const double Q, const double s, const double z, const double a2, const double a3) //immaginari
 {
-	return a4 / (sqrt(2) * Q * k * s);
+    constexpr std::complex<double> II = {0.0, 1.0};
+    return II * (-a2 + z / s * a3) / (std::sqrt(2.) * Q);
 }
 
-double f5 (const double Q, const double k, const double s, const double z, const double a5, const double a9) //immaginari
+std::complex<double> f4(const double Q, const double k, const double s, const double z, const double a4)
 {
-	return (a5 - s / z * a9) / (k * s * Q * Q);
+    return a4 / (std::sqrt(2.) * k * Q * s);
 }
 
-double f6 (const double Q, const double k, const double s, const double z, const double a1, const double a7, const double a11)
+std::complex<double> f5(const double Q, const double k, const double s, const double z, const double a5, const double a9) //immaginari
 {
-	return -(a1 / (sqrt(2)) - a7 + s / z * a11) / (k * k * Q * Q * s * s);
+    constexpr std::complex<double> II = {0.0, 1.0};
+    return -II * (a5 - s / z * a9) / (k * s * Q * Q);
 }
 
-double f7 (const double Q, const double k, const double s, const double z, const double a1, const double a6, const double a7)
+std::complex<double> f6(const double Q, const double k, const double s, const double z, const double a1, const double
+a7, const double a11)
 {
-	return -(a1 / (sqrt(2)) - a7 + s / z * a6) / (k * k * Q * Q * s * s);
+    return -(a1 / std::sqrt(2.) - a7 + s / z * a11) / (k * k * Q * Q * s * s);
 }
 
-double f8 (const double Q, const double k, const double s, const double a2, const double a8) // immaginario
+std::complex<double> f7(const double Q, const double k, const double s, const double z, const double a1, const double
+a6, const double a7)
 {
-	return (a2 / (sqrt(2)) - a8) / (k * k * Q * s * s);
+    return -(a1 / std::sqrt(2.) - a7 + s / z * a6) / (k * k * Q * Q * s * s);
+}
+
+std::complex<double> f8(const double Q, const double k, const double s, const double a2, const double a8) // immaginario
+{
+    constexpr std::complex<double> II = {0.0, 1.0};
+    return II * (a2 / std::sqrt(2.) - a8) / (k * k * Q * s * s);
 }
